621b28c8a298d4f20457390cb0ffb9faa7ff6ab3
peter-feeney <51383209+peter-feeney@users.noreply.github.com>
2019-08-11 18:45:41 -0400

    Update backend.js
    This file (for whatever reason) was missing from last commit.

---
+++ PictoMath/backend.js
@@ -0,0 +1,9 @@
+
+/** 
+Need to figure out way to restrict input to images. 
+and preferiably do it on client-side. Can't tell whether 
+or not this code works yet
+*/ 
+Dropzone.options.recommendationDiv = {
+    acceptedFiles: 'image/*'
+  };
