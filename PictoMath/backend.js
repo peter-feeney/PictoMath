c71841045107a462bac7ad1de618ab3b0359a8e9
peter-feeney <51383209+peter-feeney@users.noreply.github.com>
2019-08-13 01:06:35 -0400

    Dropzone Improvements
    Dropzone improved so that users can only input two images instead of an unlimited amount of files. Buttons generated when images uploaded (they will eventually link to functions.) Buttons need to be formatted a little bit, but we're almost there.

--- PictoMath/backend.js
+++ PictoMath/backend.js
@@ -1,9 +1,54 @@
+var picCount = 0;
 
-/** 
+Dropzone.options.uploadWidget= {
+  
+  //Change default message and make sure users can only upload images.
+  dictDefaultMessage: "Upload images here.",
+  acceptedFiles: "image/*",
+  //Restrict to only two images.
+  accept: function(file, done) {
+    console.log("uploaded");
+    done();
+  },
+  init: function() {
+    this.on("addedfile", function() {
+      if (this.files[2] != null){
+        this.removeFile(this.files[0]);
+      } else if (this.files[1] != null) {
+        $('#oneImage').toggle();
+        $('#twoImage').toggle();
+      } else if (this.files[0] != null) {
+        $('#oneImage').toggle();
+      }
+    });
+  }
+};
+
+
+
+
+/* 
 Need to figure out way to restrict input to images. 
 and preferiably do it on client-side. Can't tell whether 
 or not this code works yet
-*/ 
-Dropzone.options.recommendationDiv = {
+
+Dropzone.options.uploadFIle = {
     acceptedFiles: 'image/*'
   };
+*/ 
+
+/*
+Dropzone.options.uploadFIle = {
+  accept: function(file, done) {
+    console.log("uploaded");
+    done();
+  },
+  init: function() {
+    this.on("addedfile", function() {
+      if (this.files[1]!=null){
+        this.removeFile(this.files[0]);
+      }
+    });
+  }
+};
+*/
